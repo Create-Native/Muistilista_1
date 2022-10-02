22d563930491bed8f574795a8194b7bdf6131adf
Aki Kuivas <a-p.kvs@pm.me>
2022-10-02 11:35:37 +0300

    testi voiko työntää suoraan mainiin

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>git testi kolme</Text>
+      <Text>git testi neljä</Text>
       <StatusBar style="auto" />
     </View>
   );
